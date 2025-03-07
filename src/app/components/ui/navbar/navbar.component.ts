b37c65ef0585dbf8ad43c1013f6cbdccf0aede61
artumont <197291181+artumont@users.noreply.github.com>
2025-03-07 17:44:51 -0600

    Enhance Home and Navbar components: add showcase section, improve navigation menu, and update styles

--- src/app/components/ui/navbar/navbar.component.ts
+++ src/app/components/ui/navbar/navbar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-navbar',
-    imports: [RouterLink],
+    imports: [RouterLink, NgClass, NgIf],
     templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    isMenuOpen = false;
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
+}
