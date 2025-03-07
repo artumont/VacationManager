5f0c2d11d735ba03cc262ee98d45317baa2417b6
artumont <197291181+artumont@users.noreply.github.com>
2025-03-06 21:25:39 -0600

    Remove redundant tests for AppComponent title and rendering

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it(`should have the 'vacation-manager-ui' title`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('vacation-manager-ui');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain(
-            'Hello, vacation-manager-ui',
-        );
-    });
 });
