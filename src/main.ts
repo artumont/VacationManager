2766f206efb2e8ab538ec9590e85e4dcc72d1281
artumont <197291181+artumont@users.noreply.github.com>
2025-03-06 18:32:57 -0600

    Refactor application structure: remove Tailwind CSS setup, update component files, and adjust application routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { HomeComponent } from './app/home/home.component';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(HomeComponent)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+    .catch((err) => console.error(err));
