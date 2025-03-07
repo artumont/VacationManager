39eee8cd753acd6ce81f01f4cec6ead59c8e193c
artumont <197291181+artumont@users.noreply.github.com>
2025-03-06 18:59:45 -0600

    Refactor application structure: remove unused CSS files, update Navbar component, and configure Tailwind CSS

--- src/app/components/ui/navbar/navbar.component.ts
+++ src/app/components/ui/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    imports: [],
+    templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
 
