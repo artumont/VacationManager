d263fb4a4a901251e584845a838f74769f08b293
artumont <197291181+artumont@users.noreply.github.com>
2025-03-07 23:21:08 -0600

    Add footer component to app layout with attribution message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/ui/navbar/navbar.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './app.component.html',
 })
 export class AppComponent {
