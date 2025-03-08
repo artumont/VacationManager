9d26976328cbbf86c1a2b1a195fd4e57c1cde79a
artumont <197291181+artumont@users.noreply.github.com>
2025-03-07 23:15:01 -0600

    Add routes for employee, manager, and admin vacation management; update home component links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { RequestVacationsComponent } from './routes/employee/request-vacations/request-vacations.component';
+import { OngoingVacationsComponent } from './routes/employee/ongoing-vacations/ongoing-vacations.component';
+import { PastVacationsComponent } from './routes/employee/past-vacations/past-vacations.component';
+import { ApproveVacationsComponent } from './routes/manager/approve-vacations/approve-vacations.component';
+import { ManageVacationsComponent } from './routes/manager/manage-vacations/manage-vacations.component';
+import { ManagePermissionsComponent } from './routes/admin/manage-permissions/manage-permissions.component';
+import { ConfigureHolidaysComponent } from './routes/admin/configure-holidays/configure-holidays.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    
+    // @important: Employee
+    { path: 'employee/request-vacations', component: RequestVacationsComponent },
+    { path: 'employee/ongoing-vacations', component: OngoingVacationsComponent },
+    { path: 'employee/past-vacations', component: PastVacationsComponent },
+    
+    // @important: Manager
+    { path: 'manager/approve-vacations', component: ApproveVacationsComponent },
+    { path: 'manager/manage-vacations', component: ManageVacationsComponent },
+    
+    // @important: Admin 
+    { path: 'admin/manage-permissions', component: ManagePermissionsComponent },
+    { path: 'admin/configure-holidays', component: ConfigureHolidaysComponent }
 ];
