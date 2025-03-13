e364b15e254862ca6c18245f2f97cad01655fa69
artumont <197291181+artumont@users.noreply.github.com>
2025-03-13 08:48:42 -0600

    Implement year management in ConfigureHolidaysComponent with form validation, addition, and deletion functionality

--- src/app/routes/admin/configure-holidays/configure-holidays.component.ts
+++ src/app/routes/admin/configure-holidays/configure-holidays.component.ts
@@ -1,10 +1,47 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule, Plus, Minus } from 'lucide-angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-configure-holidays',
-    imports: [],
+    imports: [ReactiveFormsModule, NgFor, NgClass, LucideAngularModule],
     templateUrl: './configure-holidays.component.html',
 })
 export class ConfigureHolidaysComponent {
+    readonly PlusIcon = Plus;
+    readonly MinusIcon = Minus;
 
+    years: {year: string, dates:string}[] = [
+        {year: '2021', dates: '2021-01-01,2021-12-31'},
+        {year: '2022', dates: '2022-01-01,2022-12-31'},
+        {year: '2023', dates: '2023-01-01,2023-12-31'},
+        {year: '2024', dates: '2024-01-01,2024-12-31'},
+    ];
+
+    yearForm: FormGroup
+    
+    constructor(private fb: FormBuilder) {
+        this.yearForm = this.fb.group({
+            year: ['2025', Validators.required],
+        });
+    }
+
+    addYear() {
+        if (this.yearForm.valid) {
+            // @note: On a real application, this would be a service call to the backend, for now we will just log the data and add it to a list in frontend
+            this.years.push({year: this.yearForm.value.year, dates: `${this.yearForm.value.year}-01-01,${this.yearForm.value.year}-12-31`});
+            console.log('Form Data:', this.yearForm.value);
+            alert(`Year Added!\n\n${Object.entries(this.yearForm.value).map(([key, value]) => `${key}: ${value}`).join('\n')}`);
+        }
+    }
+
+    deleteYear(year: string) {
+        if (this.yearForm.valid) {
+            // @note: On a real application, this would be a service call to the backend, for now we will just log the data and remove it from the list in frontend
+            this.years = this.years.filter(y => y.year !== year)
+            console.log('Form Data:', this.yearForm.value);
+            alert(`Year Removed!\n\n${Object.entries(this.yearForm.value).map(([key, value]) => `${key}: ${value}`).join('\n')}`);
+        }
+    }
 }
