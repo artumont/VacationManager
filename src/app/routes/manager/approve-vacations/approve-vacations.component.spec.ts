c5e79974798450d0b7c8815a7092487ff9c4e8b6
artumont <197291181+artumont@users.noreply.github.com>
2025-03-14 19:49:34 -0600

    Refactor test indentation and formatting in ApproveVacationsComponent spec file for improved readability

--- src/app/routes/manager/approve-vacations/approve-vacations.component.spec.ts
+++ src/app/routes/manager/approve-vacations/approve-vacations.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApproveVacationsComponent } from './approve-vacations.component';
 
 describe('ApproveVacationsComponent', () => {
-  let component: ApproveVacationsComponent;
-  let fixture: ComponentFixture<ApproveVacationsComponent>;
+    let component: ApproveVacationsComponent;
+    let fixture: ComponentFixture<ApproveVacationsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ApproveVacationsComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ApproveVacationsComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(ApproveVacationsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ApproveVacationsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
