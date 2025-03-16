bbcb626a5fd670201cfcebb8a35ed59ff1284025
artumont <197291181+artumont@users.noreply.github.com>
2025-03-16 12:11:47 -0600

    Remove Manage Vacations component and associated routes from the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { RequestVacationsComponent } from './routes/employee/request-vacations/request-vacations.component';
 import { ApproveVacationsComponent } from './routes/manager/approve-vacations/approve-vacations.component';
-import { ManageVacationsComponent } from './routes/manager/manage-vacations/manage-vacations.component';
 import { ManagePermissionsComponent } from './routes/admin/manage-permissions/manage-permissions.component';
 import { ConfigureHolidaysComponent } from './routes/admin/configure-holidays/configure-holidays.component';
 import { VacationDashboardComponent } from './routes/employee/vacation-dashboard/vacation-dashboard.component';
@@ -16,7 +15,6 @@ export const routes: Routes = [
     
     // @important: Manager
     {path: 'manager/approve-vacations', component: ApproveVacationsComponent},
-    {path: 'manager/manage-vacations', component: ManageVacationsComponent},
     
     // @important: Admin 
     {path: 'admin/manage-permissions', component: ManagePermissionsComponent},
